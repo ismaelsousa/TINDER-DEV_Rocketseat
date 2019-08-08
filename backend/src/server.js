c324bedf6855c49eab4c2eb88113fef832f03d44
ismaesousa <ismaesousa1998@outlook.com>
2019-08-07 21:38:22 -0300

    add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes");
 
 const server = express();
-mongoose.connect('mongodb+srv://omnistack:denilson7@cluster0-4owi2.mongodb.net/omnistack8?retryWrites=true&w=majority',{useNewUrlParser:true});
+mongoose.connect(
+  "mongodb+srv://omnistack:denilson7@cluster0-4owi2.mongodb.net/omnistack8?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
+serve.use(cors());
 server.use(express.json());
 server.use(routes);
 
-server.listen(3333, ()=>{
-    console.log('serve rodando')
+server.listen(3333, () => {
+  console.log("serve rodando");
 });
