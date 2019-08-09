c9c81afdda94e856d281d8976b4cde842fb2fb2f
ismaesousa <ismaesousa1998@outlook.com>
2019-08-08 21:29:15 -0300

    falta terminar a aula

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,6 +1,49 @@
 import React from 'react'
-export default function Main(){
+import './Main.css'
+import logo from "../assets/logo.svg";
+import like from "../assets/like.svg";
+import dislike from "../assets/dislike.svg";
+export default function Main({match}){
   return (
-    <h1>main</h1>
+    <div className='main-container'>
+      <img src={logo} alt="Tindev"/>
+      <ul>
+        <li>
+          <img src="https://avatars2.githubusercontent.com/u/28990749?v=4" alt=""/>
+          <footer>
+            <strong>ismael</strong>
+            <p>menino show</p>
+          </footer>
+
+          <div className="buttons">
+            <button type='button'>
+              <img src={like} alt=""/>
+            </button>
+            <button type='button'>
+              <img src={dislike} alt=""/>
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="https://avatars2.githubusercontent.com/u/28990749?v=4" alt=""/>
+          <footer>
+            <strong>ismael</strong>
+            <p>menino show</p>
+          </footer>
+
+          <div className="buttons">
+            <button type='button'>
+              <img src={like} alt=""/>
+            </button>
+            <button type='button'>
+              <img src={dislike} alt=""/>
+            </button>
+          </div>
+        </li>
+      </ul>
+
+      
+    </div>
   );
 }
